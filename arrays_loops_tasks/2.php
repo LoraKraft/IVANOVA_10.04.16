97ee7172b6b65ba925c20c590de1b1b3f6057f9b
LoraKraft <evona@mail.ru>
2016-04-14 19:26:01 +0300

    arrays_loops_tasks/2.php

--- arrays_loops_tasks/2.php
+++ arrays_loops_tasks/2.php
@@ -1,10 +1,9 @@
 <?php
 //<p>2. Дан массив с элементами 1, 20, 15, 17, 24, 35. С помощью цикла foreach найдите сумму элементов этого массива.
 // Запишите ее в переменную $result.</p>
-
+$result = 0;
 $arr = array (1, 20, 15, 17, 24, 35);
 foreach ($arr as $val) {
- $result = array_sum($arr);
- echo "Amount arrays elements {$result}"; break;
+ $result += $val;
 }
-
+echo "Amount arrays elements {$result}";
--- arrays_loops_tasks/3.php
+++ arrays_loops_tasks/3.php
@@ -2,9 +2,13 @@
 //<p>3. Дан массив с элементами 26, 17, 136, 12, 79, 15. С помощью цикла foreach найдите сумму квадратов элементов этого массива.
 // Результат запишите переменную $result.</p>
 
+$result = 0;
+$arr2 = array();
 $arr = array (26, 17, 136, 12, 79, 15);
 foreach ($arr as $val) {
-    $result = pow ($val, 2);
-
+    $square = pow ($val, 2);
+    $result += $square;
 }
+$arr2[] = $square;
 
+echo "Amount arrays elements {$result}";
